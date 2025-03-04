e401739ca7afa2b8130c666371e6ea2670f49d5e
dlensk <djlenskold@yahoo.com>
2025-03-03 19:26:40 -0700

    Assignment 5 working

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,230 +1,199 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>
+#include <syslog.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <sys/wait.h>
 #include <signal.h>
-#include <syslog.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
+int srvrSock = -1;
+int cliSock = -1;
 #define DATAFILEPATH "/var/tmp/aesdsocketdata"
-
-#define BACKLOG 10 
-#define BUFFERSIZE 1024
 #define PORT "9000"
+#define BACKLOG 10
+#define BUFFERSIZE 1024
 
-int sockfd = -1;
-int new_fd = -1;;        	
-int terminate_connection = 0;
 
-void SignalHandler(int signal)
+void StartDaemon() 
 {
-	if (signal == SIGTERM || signal == SIGINT)
-	{
-		syslog(LOG_INFO, "Caught signal, exiting");		
-		if (sockfd != -1)
-		{
-		close(sockfd);
-		}
-		if (new_fd != -1)
-		{
-		close(new_fd);
-		}
-		terminate_connection = 1;
-		remove(DATAFILEPATH);
-		exit(0);
-	}
-	
+    pid_t pid = fork();
+    
+    if ( pid == -1 ) 
+    {
+        syslog(LOG_ERR, "Unable to fork");
+        exit(EXIT_FAILURE);
+    }
+    
+    if ( pid > 0 ) 
+    {
+        exit(EXIT_SUCCESS);
+    }
+    
+    if ( setsid() == -1 ) 
+    {
+        syslog(LOG_ERR, "Unable to create session");
+        exit(EXIT_FAILURE);
+    }
+    
+    if ( chdir("/") == -1 )
+    {
+        syslog(LOG_ERR, "Unable to chdir to root");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    close(STDERR_FILENO);
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
 }
 
-void StartDaemon()
+void SigHandler( int sig )
 {
-	printf("Attempting to fork");
-	pid_t pid = fork();
-	
-	if (pid == -1) 
-	{
-		syslog(LOG_ERR, "Fork was unsuccessfull");
-		printf("Fork was unsuccessfull");
-		exit(EXIT_FAILURE);
-	}
-	
-	printf("pid greater than 0");
-	
-	if (pid > 0) 
-	{		
-		exit(EXIT_SUCCESS);
-	}
-	
-	umask(0);
-	printf("setID error");
-	
-	if (setsid() == -1)
-	{
-		syslog(LOG_ERR, "Session creation was unsuccessful");
-		exit(EXIT_FAILURE);
-	}
-	
-	
-	if (chdir("/") == -1)
-	{
-		syslog(LOG_ERR, "Unable to change to root directory");
-		exit(EXIT_FAILURE);
-	}
-		
-	int devNull = open("/dev/null", O_RDWR);
-	
-	if(devNull == -1)
-	{
-		syslog(LOG_ERR, "Unable to open /dev/null file");
-		exit(EXIT_FAILURE);
-	}
-	
-	dup2(devNull, STDOUT_FILENO);
-	dup2(devNull, STDERR_FILENO);
-	dup2(devNull, STDIN_FILENO);
-	//close(STDOUT_FILENO);
-	//close(STDERR_FILENO);
-	//close(STDIN_FILENO);
-	close(devNull);
-	
-
+    printf("Caught signal, exiting\n");
+    
+    if (srvrSock != -1)
+    {
+    close(srvrSock);
+    }
+    
+    if (cliSock != -1) 
+    {
+    close(cliSock);
+    }
+    
+    remove(DATAFILEPATH);
+    
+    exit(0);
 }
 
 
-int main( int argc, char *argv[] )
-{
-	int daemonActive = 0;
-	char dataBuff[BUFFERSIZE];
-	struct addrinfo hints, *servinfo;
-        struct sockaddr_in clientAddr; // connector's address information
-        socklen_t addr_size = sizeof(clientAddr);
-        
-        int yes=1; 
-        
-        printf("Welcome to Main");
-        
-        signal(SIGTERM, SignalHandler);
-        signal(SIGINT, SignalHandler);
-        
-        if ((strcmp(argv[1], "-d") == 0) && argc == 2)
+int main( int argc, char *argv[] ) 
+{    
+    char dataBuffer[BUFFERSIZE];  
+    int yes = 1;
+    struct addrinfo hints, *srvrInfo;
+    struct sockaddr_in cliAddr;
+    int startDaemon = 0;
+    socklen_t client_len = sizeof(cliAddr);    
+    signal(SIGTERM, SigHandler);
+    signal(SIGINT, SigHandler);
+    
+    
+    for (int arg = 1; arg < argc; arg++)
+    {
+        if ( strcmp(argv[arg], "-d" ) == 0 ) 
         {
-        	daemonActive = 1;
-        	printf("Daemon Active");
+            startDaemon = 1;
+        }
+    }
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_STREAM;
+    hints.ai_flags=AI_PASSIVE;
+
+    if (getaddrinfo(NULL, PORT, &hints, &srvrInfo) != 0) 
+    {
+        syslog(LOG_ERR, "Unable to get address info");
+        return -1;
+    }
+
+    if ( ( srvrSock = socket( srvrInfo->ai_family, srvrInfo->ai_socktype, srvrInfo->ai_protocol ) ) == -1 ) 
+    {
+        syslog(LOG_ERR, "Unable to create socket");
+        freeaddrinfo(srvrInfo);
+        return -1;
+    }
+
+    if ( setsockopt( srvrSock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes) ) == -1 ) 
+    {
+        syslog(LOG_ERR, "Socket options not set");
+        close(srvrSock);
+        freeaddrinfo(srvrInfo);
+        return -1;
+    }
+
+    if ( bind( srvrSock, srvrInfo->ai_addr, srvrInfo->ai_addrlen ) == -1 ) 
+    {
+        syslog(LOG_ERR, "Unable to bind to socket");
+        close(srvrSock);
+        freeaddrinfo(srvrInfo);
+        return -1;
+    }
+    
+    freeaddrinfo(srvrInfo);
+
+    if (startDaemon) 
+    {
+        StartDaemon();
+    }
+
+    if ( listen( srvrSock, BACKLOG ) == -1 ) 
+    {
+        syslog(LOG_ERR, "Unable to listen");
+        return -1;
+    }
+
+    while ( 1 ) 
+    {
+        cliSock = accept( srvrSock, ( struct sockaddr * )&cliAddr, &client_len );
+        
+        if (cliSock == -1) 
+        {
+            syslog(LOG_ERR, "Unable to accept connection");
+            continue;
+        }
+
+        printf("Accepted connection from %s\n", inet_ntoa(cliAddr.sin_addr));
+
+
+        int fileDesc = open(DATAFILEPATH, O_CREAT | O_WRONLY | O_APPEND, 0666);
+        
+        if (fileDesc == -1) 
+        {
+            syslog(LOG_ERR, "Unable to gopen file");
+            close(cliSock);
+            continue;
+        }
+
+	//Read until new line 
+        ssize_t rcvBytes;
+        
+        while ( ( rcvBytes = recv( cliSock, dataBuffer, BUFFERSIZE - 1, 0 ) ) > 0 ) 
+        {
+            dataBuffer[rcvBytes] = '\0';
+            write( fileDesc, dataBuffer, rcvBytes) ;
+            if ( strchr( dataBuffer, '\n' ) )
+            { 
+            break;
+            }
         }
         
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
+        close(fileDesc);
+
+	//Send back
+        fileDesc = open(DATAFILEPATH, O_RDONLY);
         
-        if ( getaddrinfo(NULL, PORT, &hints, &servinfo) != 0)
+        if ( fileDesc != -1 ) 
         {
-        	syslog(LOG_ERR, "Unable to get address info");
-        	return -1;
-        } 
-        
-        if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
-        {
-        	syslog(LOG_ERR, "Socket creation unsuccessful");
-        	return -1;
+            while ( ( rcvBytes = read( fileDesc, dataBuffer, BUFFERSIZE ) ) > 0 ) 
+            {
+                send(cliSock, dataBuffer, rcvBytes, 0);
+            }
+            
+            close(fileDesc);
         }
+
+        printf( "Closed connection from %s\n", inet_ntoa( cliAddr.sin_addr ) );
         
-        if ( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int) ) == -1)
-        {
-        	syslog(LOG_ERR, "Socket binding unsuccessful");
-        	close(sockfd);
-        	freeaddrinfo(servinfo);
-        	return -1;
-        }
-        
-        if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
-        {
-        	syslog(LOG_ERR, "Socket binding unsuccessful");
-        	close(sockfd);
-        	freeaddrinfo(servinfo);
-        	return -1;
-        }
-        
-        freeaddrinfo(servinfo);
-             
-        if (listen(sockfd, BACKLOG) == -1)
-        {
-        	syslog(LOG_ERR, "Unable to listen on socket");
-        	//remove(DATAFILEPATH);
-        	return -1;	
-        }
-        
-        //Check if daemon needs to be started
-        if (daemonActive)
-        {
-        	printf("Daemon Starting");
-        	StartDaemon();
-        }
-         
-        while(!terminate_connection)
-        {
-        	
-        	
-        	new_fd = accept(sockfd, (struct sockaddr *)&clientAddr, &addr_size);
-        	if (new_fd == -1) 
-        	{
-            		syslog(LOG_ERR, "Connection not accepted");
-            		continue;     
-            	}     
-            	
-            	printf("Accepted connection from %s\n", inet_ntoa(clientAddr.sin_addr));
-            	
-            	int file_desc = open(DATAFILEPATH, O_CREAT | O_WRONLY | O_APPEND, 0666);
-            	if (file_desc == -1)
-            	{
-            		syslog(LOG_ERR, "Unable to open file");
-            		printf("Unable to open file");
-            		close(new_fd);
-            		continue;
-            	}
-            	
-            	
-            	ssize_t recvBytes;
-            	while ((recvBytes = recv(new_fd, dataBuff, BUFFERSIZE-1, 0)) > 0)
-            	{
-            		dataBuff[recvBytes] = '\0';
-            		write(file_desc, dataBuff, recvBytes);
-            		if (strchr(dataBuff, '\n'))
-            		{
-            		break;
-            		}
-            	
-            	}        	
-            	close(file_desc);
-            	
-            	file_desc = open(DATAFILEPATH, O_RDONLY);
-            	
-            	if (file_desc != -1)
-            	{
-            		while ( (recvBytes = read(file_desc, dataBuff, BUFFERSIZE)) > 0 )
-            		{
-            			send(new_fd, dataBuff, recvBytes, 0);
-            		}
-            		
-            		printf("Closed connection from %s\n", inet_ntoa(clientAddr.sin_addr));
-            		close(file_desc);
-            	} 
-            	
-            	close(new_fd);	         	        	      		
-        }
-      
-        //remove(DATAFILEPATH);
-        
-	return 0;       
+        close(cliSock);
+    }
+
+    return 0;
 }
-
-
-
